d52c956e290ab11d6328c8245b0072612ab33df5
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-13 16:58:04 +0200

    main.js: add import fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// import fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+library.add(faUserSecret);
+
+const app = createApp(App);
+app..component('font-awesome-icon', FontAwesomeIcon);
+app.mount("#app");
